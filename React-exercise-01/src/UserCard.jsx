8863e39bc8e023265554246f461f1f7a906c0ad0
absherGitHup <absherissa@gmail.com>
2026-06-30 11:19:08 +0300

    React-exercise-01

---
+++ React-exercise-01/src/UserCard.jsx
@@ -0,0 +1,9 @@
+const UserCard = ()=>{
+    return(
+        <div className="flex flex-col items-center mt-30 p-10 border w-100 m-auto rounded-xl border-gray-200 bg-gray-200 hover:bg-gray-100    ">
+            <h2 className="text-2xl uppercase font-bold text-gray-900 hover:text-blue-400">Abshir Ise</h2>
+            <p className="text-gray-600 hover:text-blue-400">abshirise@outlook.com</p>
+        </div>
+    )
+}
+export default UserCard
